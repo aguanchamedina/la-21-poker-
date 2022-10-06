4eb7283fa6cebfa600948767b6f1962385e83c9c
aguanchamedina <103070625+aguanchamedina@users.noreply.github.com>
2022-10-06 10:48:39 -0500

    Update 21.py

--- 21.py
+++ 21.py
@@ -1,74 +1,90 @@
-import random
+from random import sample
 
+#INPUT 
 print ("----------------------------")
 print ("--------LA 21 POKER---------")
 print ("----------------------------")
 
-#INPUT 
-jugador=random.randint(1,10)
-pc=random.randint(1,10)
-
-#Numeros aleatorios tanto del jugador como del pc"Representando las cartas del juego"
-#sum jugador
-#dan pac
-
 #PROCESS 
-suma_de_puntos_tallador=0
-suma_de_puntos_Jugador=0
-i=1
-diferencia=suma_de_puntos_tallador-suma_de_puntos_Jugador
-while i<= 2:
-    suma_de_puntos_Jugador=suma_de_puntos_Jugador+jugador
-    suma_de_puntos_tallador=suma_de_puntos_tallador+pc
-    i=i+1
-print("pc",suma_de_puntos_tallador)
-print("Jugador",suma_de_puntos_Jugador)
-print("¿Quieres seguir jugando?")
+def juego(lJugador, lCasa, lista):
+    print(lJugador)
+    if(len(lJugador)==0 and len(lCasa)==0):
+        repartirIni(lJugador, lCasa, lista)
+        return "primera"
+    else:
+        if(contador(lJugador) <= 21):
+            if(input("Desea continuar JUGADOR? (YES/NO)").upper() != "N"):
+                repartir(lJugador,lCasa,lista,0)
+            else:
+                print ("JUGADOR SU PUNTAJE ES " ,comprobacion(lJugador,contador(lJugador)))
+                repartir(lJugador,lCasa,lista,1)
+        else:
+            return print("Perdio el JUGADOR , tiene: " + str(comprobacion(lJugador,contador(lJugador))))
 
-X=int(input("1. para seguir jugando o 2. para parar: "))
+def juego1(lJugador, lCasa, lista):
+    if(comprobacion(lCasa,contador(lCasa)) <= 21):
+        print ("CASA SU PUNTAJE ES " ,comprobacion(lCasa,contador(lCasa)))
+        if(comprobacion(lCasa,contador(lCasa)) < comprobacion(lJugador,contador(lJugador))):
+            repartir(lJugador,lCasa,lista,1)
+        elif(comprobacion(lCasa,contador(lCasa))) >= comprobacion(lJugador,contador(lJugador)):
+            print("La casa gano: " + str(comprobacion(lCasa,contador(lCasa))) + " a " + str(comprobacion(lJugador,contador(lJugador))))
+            return "final"
+        elif(comprobacion(lJugador,contador(lJugador)) >= comprobacion(lCasa,contador(lCasa))):
+            print("El jugador gano: " + str(comprobacion(lJugador,contador(lJugador))) + " a " + str(comprobacion(lCasa,contador(lCasa))))
+            return "final"
 
-if X==1: 
-    while suma_de_puntos_Jugador<=21 and X!=2:
-     
-        d= random.randint(1,10)
-        
-        suma_de_puntos_Jugador=suma_de_puntos_Jugador +d 
-        print("jugador",suma_de_puntos_Jugador)
-        if suma_de_puntos_Jugador>21:
-            print("volaste")
-            break
-       
-        print("¿Quieres seguir jugando?")
-        X=int(input("1. para seguir jugando o 2. para parar: "))
-        if suma_de_puntos_Jugador>21:
-            print("volaste")
-            break
-        elif X==2:
-            while suma_de_puntos_tallador<=suma_de_puntos_Jugador and suma_de_puntos_tallador< 21:
-                p=random.randint(1,10)
-                suma_de_puntos_tallador = suma_de_puntos_tallador + p
-                print("Tallador", suma_de_puntos_tallador)
-            if suma_de_puntos_tallador>suma_de_puntos_Jugador and suma_de_puntos_tallador<21:
-                if suma_de_puntos_tallador-suma_de_puntos_Jugador==1:
-                    print("El tallador gana por un punto")
-                else:
-                    print("gana el tallador por" , suma_de_puntos_tallador-suma_de_puntos_Jugador, "Puntos")
-            elif suma_de_puntos_tallador==suma_de_puntos_Jugador:
-                print("gana el pc por ser el tallador")
-            if suma_de_puntos_tallador>21:
-                print("El tallador ah volado")
+    else:
+        return print("Perdio la CASA  tiene: " + str(comprobacion(lCasa,contador(lCasa))))
 
-elif X==2:
-            while suma_de_puntos_tallador<=suma_de_puntos_Jugador and suma_de_puntos_tallador< 21:
-                p=random.randint(1,10)
-                suma_de_puntos_tallador = suma_de_puntos_tallador + p
-                print("Tallador", suma_de_puntos_tallador)
-            if suma_de_puntos_tallador>suma_de_puntos_Jugador and suma_de_puntos_tallador<21:
-                if suma_de_puntos_tallador-suma_de_puntos_Jugador==1:
-                    print("El tallador gana por un punto")
-                else:
-                    print("gana el tallador por" , suma_de_puntos_tallador-suma_de_puntos_Jugador, "Puntos")
-            elif suma_de_puntos_tallador==suma_de_puntos_Jugador:
-                print("gana el pc por ser el tallador")
-            if suma_de_puntos_tallador>21:
-                print("El tallador ah volado")
+
+def creadorbaraja():
+    return sample([(x,y)for x in ['A',2,3,4,5,6,7,8,9,10,'J','Q','K']for y in ['DIAMANTES','TREBOLES','PICAS','CORAZONES']], 52)
+
+def valor(carta):
+    if carta[0] == 'J' or  carta[0] == 'K' or  carta[0] == 'Q':
+        return 10
+    elif carta[0] == 'A':
+            return 1
+
+    else:
+        return carta[0]
+
+def contador(lista):
+    if(len(lista)==0):
+        return 0
+    else:
+        return contador(lista[1:])+valor(lista[0])
+
+def comprobacion(lista,numero):
+    if lista == []:
+        return numero
+    elif lista[0] in [('A', 'CORAZONES'),('A', 'DIAMANTES'),('A', 'PICAS'),('A', 'TREBOLES')] and numero+10<22:
+        return comprobacion(lista[1:],numero+10)
+    else:
+        return comprobacion(lista[1:],numero)
+
+def repartirIni(lJugador, lCasa, lista):
+    lJugador.append(lista[0])
+    lJugador.append(lista[20])
+    lCasa.append(lista[1])
+    print("Cartas jugador: " + str(lJugador))
+    print("Cartas casa: " + str(lCasa))
+    juego(lJugador, lCasa, lista[4:])
+
+def repartir(lJugador, lCasa, lista,turno):
+    if turno==0:
+        lJugador.append(lista[0])
+        print("Cartas jugador: " + str(lJugador))
+        print("Cartas casa: " + str(lCasa))
+        juego(lJugador, lCasa, lista[2:])
+    if turno==1:
+        lCasa.append(lista[1])
+        print("Cartas jugador: " + str(lJugador))
+        print("Cartas casa: " + str(lCasa))
+        juego1(lJugador, lCasa, lista[2:])
+while True:
+  juego([],[],creadorbaraja())
+  if(input("Desea continuar JUGANDO BLACK JACK? (Y/N)").upper() != "Y"):
+    break
+
+#OUTPUT
